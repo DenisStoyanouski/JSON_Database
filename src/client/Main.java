f0632367c5be3e4570f364809bd3e53a12f362c6
Denis Stoyanouski <vandenst@tut.by>
2023-02-11 15:18:06 +0300

    add Socket on client side

--- src/client/Main.java
+++ src/client/Main.java
@@ -1,8 +1,33 @@
 package client;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class Main {
 
+    private static final String SERVER_ADDRESS = "127.0.0.1";
+    private static final int SERVER_PORT = 23456;
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Client started!");
+        try (
+                Socket socket = new Socket(InetAddress.getByName(SERVER_ADDRESS), SERVER_PORT);
+                DataInputStream input = new DataInputStream(socket.getInputStream());
+                DataOutputStream output  = new DataOutputStream(socket.getOutputStream())
+        ) {
+            //for (int i = 0; i < 5; i++) {
+            //Scanner scanner = new Scanner(System.in);
+            String msg = "Give me everything you have!";// scanner.nextLine();
+            System.out.printf("Sent: %s%n", msg);
+            output.writeUTF(msg);
+            String receivedMsg = input.readUTF();
+            System.out.printf("Received: %s%n",receivedMsg);
+            //}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
