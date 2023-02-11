fc003ab6ea191b439f0d45184a667b6869010e22
Denis Stoyanouski <vandenst@tut.by>
2023-02-11 15:11:28 +0300

    add simple database

---
+++ src/server/Database.java
@@ -0,0 +1,43 @@
+package server;
+
+import java.util.Arrays;
+
+class Database {
+    private String[] database = new String[100];
+
+    public Database() {
+        Arrays.fill(database, "");
+    }
+
+    public void getCell(int index) {
+        String str = null;
+        try {
+            if (!database[index - 1].isEmpty()) {
+                str = database[index - 1];
+                System.out.println(str);
+            } else throw new IndexOutOfBoundsException();
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("ERROR");
+        }
+
+    }
+
+    public void setCell(int index, String value) {
+        try {
+            database[index - 1] = value;
+            System.out.println("OK");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("ERROR");
+        }
+    }
+
+    public void deleteCell(int index) {
+        try {
+            database[index - 1] = "";
+            System.out.println("OK");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("ERROR");
+        }
+    }
+
+}
---
+++ src/server/Main.java
@@ -0,0 +1,55 @@
+package server;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static String method;
+    static int index;
+    static String value;
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        makeResponse();
+
+    }
+
+    private static void readInputFromConsole() {
+        StringBuilder sb = new StringBuilder();
+        String[] line = scanner.nextLine().split("\\s+");
+        for (int i = 0; i < line.length; i++) {
+            if (i == 0) {
+                method = line[i];
+            }
+            if (i == 1) {
+                index = Integer.parseInt(line[i]);
+            }
+            if (i > 1) {
+                sb.append(line[i]);
+                sb.append(" ");
+            }
+        }
+        value = sb.toString();
+    }
+
+    private static void makeResponse() {
+        Database db = new Database();
+        while (true) {
+            readInputFromConsole();
+            switch (method) {
+                case "get" : db.getCell(index);
+                    break;
+                case "set" : db.setCell(index, value);
+                    break;
+                case "delete" : db.deleteCell(index);
+                    break;
+                case "exit" : System.exit(0);
+                    break;
+                default:
+                    System.out.println("Unknown command. Try again");
+                    break;
+            }
+        }
+    }
+}
